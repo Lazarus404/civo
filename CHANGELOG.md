fcb6dc019b2ea33552fc003d193a733a4dea85bf
Lee Sylvester <lee.sylvester@gmail.com>
2019-10-09 09:20:07 +0100

    Update version and changelog

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,5 @@
 # Changelog
 
 09-10-2019 - Initial commit ready for hex.pm deployment
+
+10-10-2019 - Update documentation
--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Civo.MixProject do
   def project do
     [
       app: :civo,
-      version: "0.1.0",
+      version: "0.1.1",
       elixir: "~> 1.9",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
